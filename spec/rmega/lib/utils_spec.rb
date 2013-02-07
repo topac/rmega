9e90122dbf56f57ca559d311c790ea6353a9f97b
topac <topac@users.noreply.github.com>
2013-02-07 08:33:56 +0100

    a32_to_str + test

--- spec/rmega/lib/utils_spec.rb
+++ spec/rmega/lib/utils_spec.rb
@@ -8,4 +8,12 @@ describe Rmega::Utils do
       described_class.str_to_a32(string).should == a32
     end
   end
+
+  describe '#a32_to_str' do
+    it 'returns the expected value' do
+      a32 = [1953853537, 1660944384]
+      string = "tupac" + "\x00\x00\x00"
+      described_class.a32_to_str(a32).should == string
+    end
+  end
 end
