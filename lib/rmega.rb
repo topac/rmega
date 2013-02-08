64350abddfedabfea037ce6b21b7b24c47b4e44c
topac <topac@users.noreply.github.com>
2013-02-08 11:11:56 +0100

    session class

--- lib/rmega.rb
+++ lib/rmega.rb
@@ -1,9 +1,10 @@
 require "openssl"
-# require "base64"
+require "httparty"
 require "rmega/version"
 require "rmega/utils"
 require "rmega/crypto/aes"
 require "rmega/crypto/crypto"
+require "rmega/session"
 
 module Rmega
 end
