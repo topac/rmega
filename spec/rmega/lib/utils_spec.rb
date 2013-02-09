44ccc663d9184ac5e3c4365fef3490c97aa6089f
Daniele Molteni <dani.m.mobile@gmail.com>
2013-02-09 16:14:31 +0100

    fix str_to_a32 with padding

--- spec/rmega/lib/utils_spec.rb
+++ spec/rmega/lib/utils_spec.rb
@@ -1,3 +1,4 @@
+# encoding: utf-8
 require 'spec_helper'
 
 describe Rmega::Utils do
@@ -6,7 +7,9 @@ describe Rmega::Utils do
       string = 'johnsnow'
       a32 = [1785686126, 1936617335]
       described_class.str_to_a32(string).should == a32
+    end
 
+    it 'returns the expected value' do
       string = 'sjobs@apple.com'
       a32 = [1936355170, 1933599088, 1886151982, 1668246784]
       described_class.str_to_a32(string).should == a32
