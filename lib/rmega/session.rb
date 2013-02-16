1b4f68bfbf07cb3a6bf754e2f42fc907fef0cec6
Daniele Molteni <dani.m.mobile@gmail.com>
2013-02-16 14:09:37 +0100

    refactor get_sid2 method + fix decrypt_key method

--- lib/rmega/session.rb
+++ lib/rmega/session.rb
@@ -2,7 +2,7 @@ module Rmega
   class Session
     include Commands
 
-    attr_accessor :email, :request_id, :sid, :requests_timeout
+    attr_accessor :email, :request_id, :sid, :requests_timeout, :master_key
 
     def initialize email, password_str
       self.email = email
@@ -10,6 +10,30 @@ module Rmega
       self.requests_timeout = 20
 
       login password_str
+    end
+
+    def decrypt_master_key password_str, k
+      ancrypted_key = Crypto.prepare_key_pw password_str
+      self.master_key = Crypto.decrypt_key ancrypted_key, Utils.base64_to_a32(k)
+    end
+
+    def decrypt_sid csid, privk
+      # if csid ...
+      t = Utils.mpi2b Utils.base64urldecode(csid)
+      privk = Utils.a32_to_str Crypto.decrypt_key(self.master_key, Utils.base64_to_a32(privk))
+      rsa_privk = Array.new 4
+
+      # decompose private key
+      4.times do |i|
+        l = ((privk[0].ord * 256 + privk[1].ord + 7) >> 3) + 2
+        rsa_privk[i] = Utils.mpi2b privk[0..l-1]
+        privk = privk[l..-1]
+      end
+
+      # todo - remove execjs and build the key using the ruby lib
+      # rsa_key = build_rsa_key rsa_privk
+      decrypted_t = Crypto::Rsa.decrypt t, rsa_privk
+      self.sid =  Utils.base64urlencode Utils.b2s(decrypted_t)[0..42]
     end
 
     def random_request_id
@@ -20,7 +44,8 @@ module Rmega
       uh = Crypto.stringhash Crypto.prepare_key_pw(password_str), email
       resp = request a: 'us', user: email, uh: uh
       raise "Error code received: #{resp}" if error_response?(resp)
-      self.sid = Crypto.get_sid2 password_str, resp['csid'], resp['privk'], resp['k']
+      decrypt_master_key password_str, resp['k']
+      decrypt_sid resp['csid'], resp['privk']
     end
 
     def error_response? response
