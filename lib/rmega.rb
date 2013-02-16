33e79197a8b499c4139b1d40b4a68af97d4eff02
Daniele Molteni <dani.m.mobile@gmail.com>
2013-02-16 10:51:09 +0100

    logging + error checking

--- lib/rmega.rb
+++ lib/rmega.rb
@@ -1,12 +1,23 @@
 require "openssl"
 require "httparty"
 require "execjs"
+require "json"
+require "logger"
 require "rmega/version"
 require "rmega/utils"
 require "rmega/crypto/rsa"
 require "rmega/crypto/aes"
 require "rmega/crypto/crypto"
+require "rmega/commands"
 require "rmega/session"
 
 module Rmega
+  def self.logger
+    @logger ||= begin
+      logger = Logger.new $stdout
+      logger.formatter = Proc.new { | severity, time, progname, msg| "#{msg}\n" }
+      logger.level = Logger::INFO
+      logger
+    end
+  end
 end
---
+++ lib/rmega/commands.rb
@@ -0,0 +1,8 @@
+module Rmega
+  module Commands
+    def file_list
+      files = request a: 'f', c: 1
+      # todo...
+    end
+  end
+end
