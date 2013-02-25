2c7a05ea0c7cf2dc7c1fdd174da60fa52bb7c7c5
Daniele Molteni <dani.m.mobile@gmail.com>
2013-02-25 23:21:08 +0100

    moves decrypt_sid to the Crypto module

--- lib/rmega/session.rb
+++ lib/rmega/session.rb
@@ -1,54 +1,58 @@
 module Rmega
   class Session
-    attr_accessor :email, :request_id, :sid, :requests_timeout, :master_key
+    attr_accessor :email, :request_id, :sid, :master_key
 
     def initialize email, password_str
       self.email = email
       self.request_id = random_request_id
-      self.requests_timeout = 20
 
       login password_str
     end
+
+
+    # Delegate to Rmega.options
+
+    def options
+      Rmega.options
+    end
+
+    def api_request_timeout
+      options.api_request_timeout
+    end
+
+    def api_url
+      options.api_url
+    end
+
+
+    # Cache the Storage class
 
     def storage
       @storage ||= Storage.new self
     end
 
-    def decrypt_master_key password_str, k
-      ancrypted_key = Crypto.prepare_key_pw password_str
-      self.master_key = Crypto.decrypt_key ancrypted_key, Utils.base64_to_a32(k)
-    end
 
-    def decrypt_sid csid, privk
-      # if csid ...
-      t = Utils.mpi2b Utils.base64urldecode(csid)
-      privk = Utils.a32_to_str Crypto.decrypt_key(self.master_key, Utils.base64_to_a32(privk))
-      rsa_privk = Array.new 4
-      # else if tsid (todo)
-
-      # Decompose private key
-      4.times do |i|
-        l = ((privk[0].ord * 256 + privk[1].ord + 7) >> 3) + 2
-        rsa_privk[i] = Utils.mpi2b privk[0..l-1]
-        privk = privk[l..-1]
-      end
-
-      # TODO - remove execjs and build the key using the ruby lib
-      # rsa_key = Crypto::Rsa.build_rsa_key rsa_privk
-      decrypted_t = Crypto::Rsa.decrypt t, rsa_privk
-      self.sid =  Utils.base64urlencode Utils.b2s(decrypted_t)[0..42]
-    end
-
-    def random_request_id
-      rand(1E7..1E9).to_i
-    end
+    # Login-related methods
 
     def login password_str
       uh = Crypto.stringhash Crypto.prepare_key_pw(password_str), email
       resp = request a: 'us', user: email, uh: uh
       raise "Error code received: #{resp}" if error_response?(resp)
-      decrypt_master_key password_str, resp['k']
-      decrypt_sid resp['csid'], resp['privk']
+
+      # Decrypt the master key
+      encrypted_key = Crypto.prepare_key_pw password_str
+      self.master_key = Crypto.decrypt_key encrypted_key, Utils.base64_to_a32(resp['k'])
+
+      # Generate the session id
+      self.sid = Crypto.decrypt_sid master_key, resp['csid'], resp['privk']
+    end
+
+
+    # Api requests methods
+
+
+    def random_request_id
+      rand(1E7..1E9).to_i
     end
 
     def error_response? response
@@ -62,14 +66,10 @@ module Rmega
       url << "&sid=#{sid}" if sid
       Rmega.logger.debug "POST #{url}"
       Rmega.logger.debug "#{body.inspect}"
-      response = HTTPClient.new.post url, [body].to_json
+      response = HTTPClient.new.post url, [body].to_json, timeout: api_request_timeout
       Rmega.logger.debug "#{response.code}"
       Rmega.logger.debug "#{response.body}"
       JSON.parse(response.body).first
-    end
-
-    def api_url
-      "https://eu.api.mega.co.nz/cs"
     end
   end
 end
