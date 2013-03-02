317fee9b2cdf7002a9abe529d86153c3eb0cfe09
topac <topac@users.noreply.github.com>
2013-03-02 12:49:45 +0100

    test #str_to_a32

--- spec/rmega/lib/utils_spec.rb
+++ spec/rmega/lib/utils_spec.rb
@@ -2,6 +2,12 @@
 require 'spec_helper'
 
 describe Rmega::Utils do
+  def random_a32_array
+    a32_ary = []
+    rand(0..20).times { |len| a32_ary << rand(0..2**31)*(rand(2).zero? ? 1 : -1) }
+    a32_ary
+  end
+
   describe '#str_to_a32' do
     it 'returns the expected value' do
       string = 'johnsnow'
@@ -28,6 +34,17 @@ describe Rmega::Utils do
       a32 = [1953853537, 1660944384]
       string = "tupac" + "\x00\x00\x00"
       described_class.a32_to_str(a32).should == string
+    end
+
+    it 'is the opposite of #str_to_a32' do
+      a32_ary = random_a32_array
+      str = described_class.a32_to_str a32_ary
+      described_class.str_to_a32(str).should == a32_ary
+    end
+
+    it 'has the same result if len is multiplied by 4' do
+      a32 = random_a32_array
+      described_class.a32_to_str(a32).should == described_class.a32_to_str(a32, a32.size*4)
     end
   end
 
