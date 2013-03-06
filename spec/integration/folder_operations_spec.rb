6d5f7967ad54c7ff0682fa8e01af40ffe230c12c
Daniele Molteni <dani.m.mobile@gmail.com>
2013-03-06 10:36:54 +0100

    folder creations

---
+++ spec/integration/folder_operations_spec.rb
@@ -0,0 +1,28 @@
+require 'integration_spec_helper'
+
+describe 'Folders operations' do
+  before :all do
+    @session = storage.session
+    @parent_node = @session.storage.root_node
+    @folder_name = "test_folder_#{rand.denominator}_#{rand.denominator}"
+    @folder_node = Rmega::FolderNode.create @session, @parent_node, @folder_name
+  end
+
+  it 'creates a new folder' do
+    @folder_node.should be_kind_of Rmega::Node
+  end
+
+  it 'finds the folder' do
+    node = @session.storage.nodes.find { |n| n.name == @folder_name }
+    node.should_not be_nil
+  end
+
+  it 'deletes the folder' do
+    lambda { @folder_node.delete }.should_not raise_error
+  end
+
+  it 'does not find the folder anymore' do
+    node = @session.storage.nodes.find { |n| n.name == @folder_name }
+    node.should be_nil
+  end
+end
