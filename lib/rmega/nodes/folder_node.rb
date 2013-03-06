f91a0e624389c35546c05329c9fbc04073817fed
topac <topac@users.noreply.github.com>
2013-03-06 10:36:54 +0100

    folder creations

--- lib/rmega/nodes/folder_node.rb
+++ lib/rmega/nodes/folder_node.rb
@@ -1,5 +1,14 @@
 module Rmega
   class FolderNode < Node
+    def self.create session, parent_node, folder_name
+      key = Crypto.random_key
+      encrypted_attributes = Utils.a32_to_base64 Crypto.encrypt_attributes(key[0..3], {n: folder_name.strip})
+      encrypted_key = Utils.a32_to_base64 Crypto.encrypt_key(session.master_key, key)
+      n = [{h: 'xxxxxxxx', t: 1, a: encrypted_attributes, k: encrypted_key}]
+      data = session.request a: 'p', t: parent_node.handle, n: n
+      new session, data['f'][0]
+    end
+
     def children
       storage.nodes.select { |node| node.parent_handle == handle }
     end
