fa67098ccd40a8997d558325e66f9c564252e576
Daniele Molteni <dani.m.mobile@gmail.com>
2013-03-06 10:34:50 +0100

    random_key

--- lib/rmega/crypto/crypto.rb
+++ lib/rmega/crypto/crypto.rb
@@ -2,6 +2,10 @@ module Rmega
   module Crypto
     extend self
 
+    def random_key
+      Array.new(6).map { rand(0..0xFFFFFFFF) }
+    end
+
     def prepare_key ary
       pkey = [0x93C467E3,0x7DB0C7A4,0xD1BE3F81,0x0152CB56]
       65536.times do
