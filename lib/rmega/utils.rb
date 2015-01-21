4dc08edc36bddb301c062ebb73ff17cffd0188a0
topac <topac@users.noreply.github.com>
2015-01-21 10:44:31 +0100

    use the default base64 lib

--- lib/rmega/utils.rb
+++ lib/rmega/utils.rb
@@ -1,3 +1,5 @@
+require 'base64'
+
 module Rmega
   module Utils
     extend self
@@ -34,70 +36,14 @@ module Rmega
     end
 
     def base64urlencode(string)
-      i = 0
-      tmp_arr = []
-
-      while i < string.size + 1
-        o1 = string[i].ord rescue 0
-        i += 1
-        o2 = string[i].ord rescue 0
-        i += 1
-        o3 = string[i].ord rescue 0
-        i += 1
-
-        bits = o1 << 16 | o2 << 8 | o3
-
-        h1 = bits >> 18 & 0x3f
-        h2 = bits >> 12 & 0x3f
-        h3 = bits >> 6 & 0x3f
-        h4 = bits & 0x3f
-
-        tmp_arr.push b64a[h1] + b64a[h2] + b64a[h3] + b64a[h4]
-      end
-
-      enc = tmp_arr.join ''
       r = string.size % 3
-      (r != 0) ? enc[0..r - 4] : enc
+      encoded = Base64.urlsafe_encode64(string)
+      return (r != 0) ? encoded[0..r - 4] : encoded
     end
 
     def base64urldecode(data)
-      data += '=='[((2-data.length*3)&3)..-1]
-
-      i = 0
-      ac = 0
-      dec = ""
-      tmp_arr = []
-
-      return data unless data
-
-      while i < data.size
-        h1 = b64a.index(data[i]) || -1
-        i += 1
-        h2 = b64a.index(data[i]) || -1
-        i += 1
-        h3 = b64a.index(data[i]) || -1
-        i += 1
-        h4 = b64a.index(data[i]) || -1
-        i += 1
-
-        bits = (h1 << 18) | (h2 << 12) | (h3 << 6) | h4
-
-        o1 = bits >> 16 & 0xff
-        o2 = bits >> 8 & 0xff
-        o3 = bits & 0xff
-
-        if h3 == 64
-          tmp_arr[ac] = o1.chr
-        elsif h4 == 64
-          tmp_arr[ac] = o1.chr + o2.chr
-        else
-          tmp_arr[ac] = o1.chr + o2.chr + o3.chr
-        end
-
-        ac += 1
-      end
-
-      tmp_arr.join ''
+      fix_for_decoding = '=='[((2-data.length*3)&3)..-1]
+      return Base64.urlsafe_decode64("#{data}#{fix_for_decoding}")
     end
 
     def mpi2b(s)
