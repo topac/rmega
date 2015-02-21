13561262f6d8d092bf2aee994113c825c4a2938b
topac <topac@users.noreply.github.com>
2015-02-21 15:14:30 +0100

    use NodeKey class to generate a key

--- spec/integration/folder_operations_spec.rb
+++ spec/integration/folder_operations_spec.rb
@@ -5,58 +5,57 @@ describe 'Folders operations' do
   if account_file_exists?
 
     before(:all) do
-      @name = "folder_#{rand.denominator}_#{rand.denominator}"
-      @sub_folder_name = "folder_#{rand.denominator}_#{rand.denominator}"
       @storage = login
     end
 
-    # Note: node searching is traversal
-    def find_folder(name)
-      @storage.nodes.find { |f| f.type == :folder && f.name == name }
-    end
+    let(:name) { "test_folder" }
 
     context 'when #create_folder is called on a node' do
 
+      before do
+        @folder = @storage.root.create_folder(name)
+      end
+
       it 'creates a new folder under that node' do
-        folder = @storage.root.create_folder(@name)
-        expect(folder.name).to eql @name
-        expect(folder.parent_handle).to eq @storage.root.handle
+        expect(@folder.name).to eql name
+        expect(@folder.parent_handle).to eq @storage.root.handle
+      end
+
+      after do
+        @folder.delete
       end
     end
 
-    context 'searching for a folder by its name' do
+    context 'searching for a folder by its handle' do
+      before do
+        @folder = @storage.root.create_folder(name)
+      end
 
       it 'returns the matching folder' do
-        expect(find_folder(@name).name).to eql @name
+        found_node = @storage.nodes.find { |n| n.handle == @folder.handle }
+        expect(found_node).not_to be_nil
+      end
+
+      after do
+        @folder.delete
       end
     end
 
-    context "when #create_folder under created folder" do
-
-      it "creates a new folder under created folder" do
-        parent_folder = find_folder(@name)
-
-        sub_folder = parent_folder.create_folder(@sub_folder_name)
-
-        expect(parent_folder.folders.first.name).to eql @sub_folder_name
-        expect(sub_folder.parent_handle).to eql parent_folder.handle
-      end
-    end
-
-    context 'when #delete is called on a folder node' do
-
-      it 'deletes sub folder' do
-        expect(find_folder(@sub_folder_name).delete).to eql 0
+    context 'when #create_folder under created folder' do
+      before do
+        @folder = @storage.root.create_folder(name)
+        @sub_folder = @folder.create_folder(name)
       end
 
-      # todo: i see this failing a couple of times
-      it 'deletes the folder' do
-        expect(find_folder(@name).delete).to eql 0
+      it 'creates a new folder under created folder' do
+        found_node = @storage.nodes.find { |n| n.handle == @sub_folder.handle }
+        expect(found_node.parent_handle).to eq(@folder.handle)
       end
 
-      it 'does not find the folder (and its subfolder) anymore' do
-        expect(find_folder(@name)).to be_nil
-        expect(find_folder(@sub_folder_name)).to be_nil
+      after do
+        @folder.delete
+        found_node = @storage.nodes.find { |n| n.handle == @sub_folder.handle }
+        expect(found_node).to be_nil
       end
     end
   end
