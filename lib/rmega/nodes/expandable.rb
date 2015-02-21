ef70217209b1d36236de3f34f544f4cf0d60666c
topac <dani.m.mobile@gmail.com>
2015-02-21 15:14:30 +0100

    use NodeKey class to generate a key

--- lib/rmega/nodes/expandable.rb
+++ lib/rmega/nodes/expandable.rb
@@ -4,20 +4,20 @@ module Rmega
       include Uploadable
 
       def create_folder(name)
-        key = OpenSSL::Random.random_bytes(24) #128 bit key + 64 bit nonce
+        node_key = NodeKey.random
 
         # encrypt attributes
         attributes_str = "MEGA"
         attributes_str << {n: name.strip}.to_json
         attributes_str << ("\x00" * (16 - (attributes_str.size % 16)))
-        encrypted_attributes = aes_cbc_encrypt(key[0..15], attributes_str)
+        encrypted_attributes = aes_cbc_encrypt(node_key.aes_key, attributes_str)
 
         # Encrypt node key
-        encrypted_key = aes_ecb_encrypt(session.master_key, key[0..15])
+        encrypted_key = aes_ecb_encrypt(session.master_key, node_key.aes_key)
 
         n = [{h: 'xxxxxxxx', t: 1, a: Utils.base64urlencode(encrypted_attributes), k: Utils.base64urlencode(encrypted_key)}]
         data = session.request(a: 'p', t: handle, n: n)
-        Folder.new(session, data['f'][0])
+        return Folder.new(session, data['f'][0])
       end
 
       def upload_url(filesize)
