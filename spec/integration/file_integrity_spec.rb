2c96ebf01808b3b633d31f739fe5bbf82ee4dcf6
topac <topac@users.noreply.github.com>
2015-09-18 11:32:58 +0200

    Node#rename

--- spec/integration/file_integrity_spec.rb
+++ spec/integration/file_integrity_spec.rb
@@ -1,6 +1,6 @@
 require 'integration_spec_helper'
 
-describe 'File integrity over upload/download operations' do
+describe 'File integrity' do
 
   if account_file_exists?
 
@@ -8,32 +8,39 @@ describe 'File integrity over upload/download operations' do
       @storage = login
     end
 
-    let(:name) { "test_file" }
+    context "when a file is renamed" do
 
-    let(:path) { File.join(temp_folder, name)}
+      let(:path) { "#{temp_folder}/testfile_#{SecureRandom.hex(6)}" }
 
-    [12, 1_024_000].each do |size|
+      let(:new_name) { "testfile_#{SecureRandom.hex(6)}" }
+
+      it 'it does not get corrupted' do
+        File.write(path, SecureRandom.hex(24))
+        file = @storage.root.upload(path)
+        file.rename(new_name)
+        expect(file.name).to eq(new_name)
+        file = @storage.nodes.find { |n| n.handle == file.handle }
+        file.delete
+        expect(file.name).to eq(new_name)
+      end
+    end
+
+    [12, 6_000].each do |size|
 
       context "when a file (#{size} bytes) is uploaded and then downloaded" do
 
-        let(:content) { OpenSSL::Random.random_bytes(size) }
+        let(:path) { "#{temp_folder}/testfile_#{SecureRandom.hex(6)}" }
+
+        let(:content) { SecureRandom.random_bytes(size) }
 
         let(:content_hash) { Digest::MD5.hexdigest(content) }
 
-        before do
-          File.open(path, 'wb') { |f| f.write(content) }
-          file = @storage.root.upload(path)
-          @file = @storage.nodes.find { |n| n.handle == file.handle }
-          expect(@file.name).to eq(name)
-          @file.download(path+".downloaded")
-        end
-
         it 'it does not get corrupted' do
-          expect(Digest::MD5.file(path+".downloaded").hexdigest).to eq(content_hash)
-        end
-
-        after do
-          @file.delete if @file
+          File.write(path, content)
+          file = @storage.root.upload(path)
+          file.download("#{path}.downloaded")
+          file.delete
+          expect(Digest::MD5.file("#{path}.downloaded").hexdigest).to eq(content_hash)
         end
       end
     end
--- spec/integration/file_upload_spec.rb
+++ spec/integration/file_upload_spec.rb
@@ -4,27 +4,24 @@ describe 'File upload' do
 
   if account_file_exists?
 
-    before(:all) { @storage = login }
+    before(:all) do
+      @storage = login
+    end
 
-    let(:name) { "test_file" }
+    [12, 6_000].each do |size|
 
-    let(:path) { File.join(temp_folder, name)}
-
-    [12, 1_024_000].each do |size|
       context "when a file (#{size} bytes) is uploaded" do
 
-        before do
-          File.open(path, 'wb') { |f| f.write(OpenSSL::Random.random_bytes(size)) }
-          @file = @storage.root.upload(path)
-        end
+        let(:path) { "#{temp_folder}/testfile_#{SecureRandom.hex(6)}" }
 
-        it 'it can be found as a file node' do
-          found_node = @storage.root.files.find { |f| f.handle == @file.handle }
-          expect(found_node).not_to be_nil
-        end
+        let(:content) { SecureRandom.random_bytes(size) }
 
-        after do
-          @file.delete if @file
+        it 'is found' do
+          File.write(path, content)
+          file = @storage.root.upload(path)
+          file = @storage.root.files.find { |f| f.handle == file.handle }
+          file.delete
+          expect(file).not_to be_nil
         end
       end
     end
